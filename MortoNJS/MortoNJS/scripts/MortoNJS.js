e96eb3cae0e542cb8bdf7caeb8d858f4ac097cca
Jalmer van de Berg <jalmervandeberg@sogyo.nl>
2016-06-08 08:54:06 +0200

    staged changes

---
+++ MortoNJS/MortoNJS/scripts/MortoNJS.js
@@ -0,0 +1,9 @@
+﻿function init() {
+    var canvas = document.getElementById("MortoNJS");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+    ctx.moveTo(0, 0);
+    ctx.lineTo(canvas.clientWidth, canvas.clientHeight);
+    ctx.stroke();
+}
---
+++ MortoNJS/MortoNJS/scripts/cpu.js
@@ -0,0 +1,23 @@
+﻿var cpu = function () {
+    //private
+    //registers
+    var a, x, y;
+    //program counter/stack pointer
+    var pc, sp;
+    //status bits
+    var negative, overflow, brk, bcd, disableInterupt, zero, carry;
+    var cartridge;
+    var memory;
+
+    //public
+    return {
+        create: function () {
+
+        },
+        execute: function () {
+        },
+        setCartridge: function (c) {
+        }
+
+    };
+}();
